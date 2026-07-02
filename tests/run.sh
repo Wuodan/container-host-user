1d0e71cb7218a166259d6cc19882998a4eb45773
Stefan Kuhn <stefan.kuhn.zurich@gmail.com>
2026-07-02 16:28:05 +0200

    chore(lint): fix shellcheck issues

--- tests/run.sh
+++ tests/run.sh
@@ -4,6 +4,6 @@ set -eu
 # User-facing test runner.
 # Run this script to execute the full bats suite, including setup_suite.
 
-repo_root=$(CDPATH= cd -- "$(dirname "$0")/.." && pwd)
+repo_root=$(CDPATH='' cd -- "$(dirname "$0")/.." && pwd)
 
 exec bats --recursive "${repo_root}/tests/bats"
